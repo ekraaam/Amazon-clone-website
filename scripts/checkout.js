70f7d4ae812d29bc71fbad10a1c978915450281b
Ekram Ahmad <ekramahmad560@gmail.com>
2024-01-16 19:46:04 +0530

    adding local storage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,15 +6,15 @@ let cartSummaryHtml='';
 
 cart.forEach((cartItem)=>{
     const productId =cartItem.productId;
-
     let matchingProduct;
     products.forEach((product)=>{
         if(product.id===productId){
-            matchingProduct=product;
+            matchingProduct = product;
         }
     });
     cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container
+     js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -100,5 +100,7 @@ document.querySelectorAll('.js-delete-quantity')
         link.addEventListener('click', ()=>{
             const productId=link.dataset.productId;
             removeCartItem(productId);
+            const container=document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         })
     })
