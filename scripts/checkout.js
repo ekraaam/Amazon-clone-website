04846b0dac86c7451d294c8593cb6c68fe4e61a5
Ekram Ahmad <ekramahmad560@gmail.com>
2024-01-17 19:36:03 +0530

    add delivery options object

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,11 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+const deliveryDate=dayjs();
+
+console.log(deliveryDate.add('3','days').format('dddd, MMMM D'));
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
